25cf8c4af47e561cc6778adbe2ed177bd553308b
Ali_Modami <ali.modami.alijani@gmail.com>
2026-07-08 14:32:05 +0330

    Add initial project configuration template

--- Core/App/Inc/project_config.h
+++ Core/App/Inc/project_config.h
@@ -1,13 +1,75 @@
-/*
- * project_config.h
+/******************************************************************************
+ * @file    project_config.h
+ * @brief   Global project configuration file
  *
- *  Created on: Jul 8, 2026
- *      Author: A_Modami
+ *----------------------------------------------------------------------------
+ * Project :
+ *      Dual Voltage Monitor
+ *
+ * MCU :
+ *      STM32F103C8T6 (Blue Pill)
+ *
+ * Author :
+ *      Ali Modami
+ *
+ * Description :
+ *      This file contains global project definitions.
+ *
+ *      The purpose of this file is to collect common project information
+ *      in one location, so future maintenance becomes much easier.
+ *
+ *      Every application module may include this file instead of defining
+ *      duplicated values in multiple places.
+ *
+ *----------------------------------------------------------------------------
+ * Change History
+ *
+ * Version | Date       | Description
+ * --------+------------+----------------------------------------------
+ * 1.0.0   | 2026-07-08 | Initial project configuration template
+ *
+ ******************************************************************************/
+
+#ifndef PROJECT_CONFIG_H
+#define PROJECT_CONFIG_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*==========================================================================*/
+/*                              Project Info                                */
+/*==========================================================================*/
+
+#define PROJECT_NAME               "Dual Voltage Monitor"
+
+#define PROJECT_VERSION_MAJOR      1
+#define PROJECT_VERSION_MINOR      0
+#define PROJECT_VERSION_PATCH      0
+
+/*==========================================================================*/
+/*                           Future Configuration                           */
+/*==========================================================================*/
+
+/*
+ * Future project-wide configuration values will be placed here.
+ *
+ * Examples:
+ *
+ * LCD Type
+ * ADC Filter Size
+ * UART Baudrate
+ * Flash Address
+ * Sample Time
+ * Debug Enable
+ * etc...
  */
 
-#ifndef APP_INC_PROJECT_CONFIG_H_
-#define APP_INC_PROJECT_CONFIG_H_
+/*==========================================================================*/
 
+#ifdef __cplusplus
+}
+#endif
 
-
-#endif /* APP_INC_PROJECT_CONFIG_H_ */
+#endif /* PROJECT_CONFIG_H */
