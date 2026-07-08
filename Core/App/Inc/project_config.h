0ed78b4c220cdcbf62ef495449e2c33bf088e23f
Ali_Modami <ali.modami.alijani@gmail.com>
2026-07-08 14:13:27 +0330

    Initial STM32CubeMX project generation

---
+++ Core/App/Inc/project_config.h
@@ -0,0 +1,13 @@
+/*
+ * project_config.h
+ *
+ *  Created on: Jul 8, 2026
+ *      Author: A_Modami
+ */
+
+#ifndef APP_INC_PROJECT_CONFIG_H_
+#define APP_INC_PROJECT_CONFIG_H_
+
+
+
+#endif /* APP_INC_PROJECT_CONFIG_H_ */
